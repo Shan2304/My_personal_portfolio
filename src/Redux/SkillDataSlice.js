abd798a768c0d3599a8c716338a1be6333c43f2b
shantanu kumar kushwaha <121479542+Shan2304@users.noreply.github.com>
2023-09-12 18:34:53 +0530

    Update SkillDataSlice.js

--- src/Redux/SkillDataSlice.js
+++ src/Redux/SkillDataSlice.js
@@ -8,6 +8,10 @@ import n_3_img from "../Media/tic tak toe/3.jpg";
 import ns_1_img from "../Media/hospital-hub/1.jpg";
 import ns_2_img from "../Media/hospital-hub/2.jpg";
 import ns_3_img from "../Media/hospital-hub/3.jpg";
+import ns_3_img from "../Media/r-pay1.png";
+import ns_3_img from "../Media/r-pay1.png";
+import ns_3_img from "../Media/r-pay1.png";
+
 const initialState = {
   dataList: [
     { title: "Java", value: 95 },
@@ -112,6 +116,15 @@ const initialState = {
       /*deployedIn: "----",*/
       deployLink: "https://shan2304.github.io/Health-Hub/",
       images: [ns_1_img, ns_2_img, ns_3_img],
+    },
+    {
+      title: "Razor-Pay clone",
+      responsive: "Yes",
+      codeLink: " ",
+      deployed: "Yes",
+      /*deployedIn: "https://app.netlify.com/",*/
+      deployLink: " ",
+      images: [r-pay1, r-pay2,r-pay3],
     },
     {
       title: "Weather-API",
